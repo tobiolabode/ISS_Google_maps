4bc90039d16b6f0fe0a6a444f92a8de0a7175b75
tobiolabode <tobi@tobiolabode.com>
2019-09-04 16:58:16 +0100

    Added google maps package Able to do a reverse geocode search of coordinates.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import requests
+import googlemaps
+from datetime import datetime
+
+gmaps = googlemaps.Client(key='AIzaSyC0dEPMVePlBkEaqSs6O4l-E-sOcyawvOE')
+
 
 r = requests.get('http://api.open-notify.org/iss-now.json')
 result = r.json()
 
-loaction = result['iss_position']
-latitude = loaction['latitude']
-longitude = loaction['longitude']
+location = result['iss_position']
+latitude = location['latitude']
+longitude = location['longitude']
 
 print('latitude:', latitude)
 print('longitude:', longitude)
+
+
+reverse_geocode_result = gmaps.reverse_geocode((latitude, longitude))
+print(reverse_geocode_result)
--- to_do.md
+++ to_do.md
@@ -1,3 +1,7 @@
 [ ] get apis
 [x] get iss loaction api
+[ ] get google maps api
 [ ]
+
+idea: print out the astronuts on the ISS, with infomation about them.
+with picture and bio
