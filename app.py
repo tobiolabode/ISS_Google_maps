13e8120ef848aa74fa3497bcbb4bab3836108623
tobiolabode <tobi@tobiolabode.com>
2019-09-13 14:17:29 +0100

    Adding columns areas for austonuts details

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template
-from flask_googlemaps import GoogleMaps
-from flask_googlemaps import Map
 from main import longitudefloat, latitudefloat
 
 app = Flask(__name__, template_folder=".")
